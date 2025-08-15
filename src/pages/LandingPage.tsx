50bc82111578694726f24a634f1b6f80bd59d514
Arjun <arjunsah856@gmail.com>
2025-08-15 20:42:59 +0545

    installed react, react-dom ,react-router-dom

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"; 
+import { useNavigate } from 'react-router-dom';
+
+
+export default function LandingPage() {
+  return (
+    <>
+    <div>LandingPage</div>
+    <Button />
+    </>
+  )
+}
