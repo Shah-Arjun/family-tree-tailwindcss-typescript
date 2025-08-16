e649a1579de8e6d7f6a0c0accda92ed1e985cdd0
Arjun <arjunsah856@gmail.com>
2025-08-16 14:44:48 +0545

    added auth page

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import LandingPage from './pages/LandingPage.tsx';
+import AuthPage from './pages/AuthPage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -23,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/landingpage" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
