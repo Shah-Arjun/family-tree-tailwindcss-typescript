963fb313c0c87fe93049af9a7b25988dd0af622d
Arjun <arjunsah856@gmail.com>
2025-08-27 21:11:32 +0545

    routes changed

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.tsx';
+// import App from './App.tsx';
 import './index.css';
 import LandingPage from './pages/LandingPage.tsx';
 import AuthPage from './pages/AuthPage.tsx';
@@ -23,8 +23,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/landingpage" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/landingpage" element={<LandingPage />} /> */}
         <Route path="/auth" element={<AuthPage />} />
         <Route path='/family-tree' element={<FamilyTreeApp />}></Route>
       </Routes>
