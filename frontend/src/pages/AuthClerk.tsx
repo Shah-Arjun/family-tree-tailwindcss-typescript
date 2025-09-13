844c29c6d52af46b6135a8d3f6ac108ae006ef3f
Arjun <arjunsah856@gmail.com>
2025-09-13 23:00:54 +0545

    added tailwind css to AuthClerk component

--- frontend/src/pages/AuthClerk.tsx
+++ frontend/src/pages/AuthClerk.tsx
@@ -11,13 +11,13 @@ const AuthClerk = () => {
     const [mode, setMode] = useState<"signin" | "signup">('signin')
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen gap-6">
+        <div className="flex flex-col items-center justify-center h-screen">
             {mode === 'signin' ? (
                 <>
                     <SignIn path="/auth" routing="path" signUpUrl="/auth" />
-                    <p>
+                    <p className="mt-2 text-sm">
                         Don't have an account?
-                        <button onClick={() => setMode('signup')}>
+                        <button onClick={() => setMode('signup')} className="text-blue-600 underline" >
                             Sign Up
                         </button>
                     </p>
@@ -25,9 +25,9 @@ const AuthClerk = () => {
             ) : (
                 <>
                     <SignUp path="/auth" routing="path" signInUrl="/auth" />
-                    <p>
+                    <p className="mt-4 text-sm">
                         Already have an account?
-                        <button onClick={() => setMode("signin")}>
+                        <button onClick={() => setMode("signin")} className="text-blue-600 underline">
                             Sign In
                         </button>
                     </p>
