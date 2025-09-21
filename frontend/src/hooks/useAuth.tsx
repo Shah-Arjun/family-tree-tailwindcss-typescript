75da428462fe900ccbfc075108c2272d67fe5e61
Arjun <arjunsah856@gmail.com>
2025-09-21 19:35:34 +0545

    added useAuth hook to place the logged in user in navigation bar

--- frontend/src/hooks/useAuth.tsx
+++ frontend/src/hooks/useAuth.tsx
@@ -1,18 +1,47 @@
 import { useState, useEffect } from "react"
-import jwtDecode from "jwt-decode"
+import { jwtDecode } from "jwt-decode"
 
-interface jwtPayload {
+interface JwtPayload {
   user: {
     id: string,
     email: string,
     name?: string,
-  }
+  };
+  exp: number;
 }
 
-const useAuth = () => {
+export const useAuth = () => {
+  const [user, setUser] = useState<JwtPayload['user'] | null>(null)
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if(!token) return setUser(null);
+
+    try {
+      const decoded = jwtDecode<JwtPayload>(token)
+
+      //check if token is expired
+      if(decoded.exp * 1000 > Date.now()){          // second*1000 > millisecond
+        localStorage.removeItem("token");
+        return setUser(null);
+      }
+
+      setUser(decoded.user)
+    } catch (err) {
+      console.error("Invalid token:", err)
+      localStorage.removeItem("token");
+      setUser(null)
+    }
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("token")
+    setUser(null);
+  }
+
   return (
-    
+    { user, logout }
   )
 }
 
-export default useAuth
